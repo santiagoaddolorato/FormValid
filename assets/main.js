dbb268ced14a80e7717071d55dbe1e4325526f55
Santiago Addolorato <addoloratosantiago@gmail.com>
2023-06-23 00:15:14 -0300

    secondCommit

--- assets/main.js
+++ assets/main.js
@@ -20,10 +20,68 @@ const checkUserName = ()=>{
     return valid;
 }
 
+const checkMail = ()=>{
+    let valid = false;
+    const emailValue = emailInput.value.trim();
+    if (isEmpty(emailInput)){
+        showError(emailInput, "El mail es obligatorio");
+    } else if (!isEmailValid(emailValue)){
+        showError(emailInput, "El mail no es valido") ;
+    } else {
+        showSuccess(emailInput);
+        valid = true;
+    }
+    return valid;
+}
+
+
+const checkPassword = ()=>{
+    let valid = false;
+    const password  = passInput.value.trim();
+    if(isEmpty(password)){
+        showError(passInput, "La contraseña es obligatoria")
+    } else if (!isPassSecure(password)){
+        showError(passInput, "La contraseña debe tener al menos 8 caracteres, una mayuscula, una minuscula y un simbolo");
+    } else{
+        showSuccess(passInput);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkPhone = ()=>{
+    let valid = false;
+    const phoneValue = phoneInput.value.trim();
+    if (isEmpty(phoneValue)) {
+        showError(phoneInput, "El telefono es obligatorio")
+    } else if (!isPhoneValid(phoneValue)){
+        showError(phoneInput, "El telefono ingresado no es valido")
+    } else {
+        showSuccess(phoneInput);
+        valid = true;
+    }
+    return valid;
+}
+
 const isEmpty = (value)=> value === "";
 
 const isBetween = (length, min, max)=>{
     return length < min || length > max ? false : true;
+}
+
+const isEmailValid = (email) => {
+	const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+	return re.test(email);
+}
+
+const isPassSecure = (pass) =>{
+    const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+    return re.test(pass);
+}
+
+const isPhoneValid = (phone) =>{
+    const re = /^[0-9]{10}$/;
+    return re.test(phone);
 }
 
 const showError= (input, message)=>{
@@ -41,10 +99,51 @@ const showSuccess= (input)=>{
     const error = formfield.querySelector("small");
     error.textContent = "";
 }
-
-
+const debounce = (fn, delay = 500)=>{
+    let timeoutId;
+    return (...arg)=>{
+        if(timeoutId) clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            fn.apply(null, arg);
+        }, delay);
+    };
+};
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    checkUserName();
+    let isUsernameValid = checkUserName();
+    let isEmailValidd = checkMail();
+    let isPasswordValid = checkPassword();
+    let isPhoneValidd = checkPhone();
+
+    let isFormValid = isUsernameValid && isEmailValidd && isPasswordValid && isPhoneValidd;
+    if (isFormValid){
+        form.submit();
+    }
 })
+
+form.addEventListener("input", debounce((e)=>{
+    switch (e.target.id) {
+        case "username":
+            checkUserName();
+            break;
+        case "email":
+            checkMail();
+            break;
+        case "password":
+            checkPassword();
+            break;
+        case "phone":
+            checkPhone();
+            break;
+    };
+}));
+
+
+// form.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+//     checkUserName();
+//     checkMail();
+//     checkPassword();
+//     checkPhone();
+// })
